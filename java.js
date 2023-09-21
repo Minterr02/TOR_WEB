904df96f0fc3eec16dc17de94c36986c5cdb0f65
Minterr02 <ananyaporn.minterr@gmail.com>
2023-09-21 21:06:58 +0700

    java

---
+++ java.js
@@ -0,0 +1,8 @@
+/*window.onscroll = function() {
+    var head_section = document.getElementById("head_section");
+    if (window.pageYOffset >= 100) {
+        head_section.classList.add("sticky");
+    } else {
+        head_section.classList.remove("sticky");
+    }
+  };
